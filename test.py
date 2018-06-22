f8ab449c20502d8f883268d360304b6e4fea7163
alex <alex@alexmartins.com>
2018-06-21 21:36:03 -0400

    Testando o Deploy automatico (2).

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "... ((( Devops Essentials 4Linux ))) ...")
+        self.assertRegex(result.data.decode(), "... Devops Essentials 4Linux, 2018-06-21 ...")
 
 
 if __name__ == "__main__":
